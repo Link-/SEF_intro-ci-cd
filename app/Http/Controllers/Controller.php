a64be38a1e35a5aec4246362c6e20ad2c20c83c7
Bassem Dghaidi <bassem@interop.link>
2021-06-14 21:07:10 +0200

    Add php doc blocks

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,5 +1,19 @@
 <?php
-
+/**
+ * Duis laborum pariatur reprehenderit est tempor. Commodo do est labore 
+ * enim. Ipsum sint est id in sit irure eu nulla mollit quis duis ullamco. 
+ * Eu commodo aliquip ex voluptate cupidatat quis. Duis duis pariatur in 
+ * aliquip culpa pariatur eu duis tempor anim laborum cupidatat. Do esse 
+ * excepteur excepteur esse laborum aute. Officia nulla magna ad incididunt 
+ * reprehenderit consequat voluptate.
+ * 
+ * @category    Some_Category
+ * @package     Laravel
+ * @author      Link- <bassem@interop.link>
+ * @license     MIT https://github.com/Link-/SEF_intro-ci-cd/blob/main/LICENSE
+ * @link        https://github.com/Link-/SEF_intro-ci-cd
+ * @php_version 8.0
+ */
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -7,6 +21,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * Duis laborum pariatur reprehenderit est tempor. Commodo do est labore
+ * 
+ * @category Some_Category
+ * @package  Laravel
+ * @author   Link- <bassem@interop.link>
+ * @license  MIT https://github.com/Link-/SEF_intro-ci-cd/blob/main/LICENSE
+ * @link     https://github.com/Link-/SEF_intro-ci-cd
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
